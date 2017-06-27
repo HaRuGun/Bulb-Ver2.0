d11ad8885a338348828465bfc7ef2fad6971ec7d
HaRu <vharugun9093@gmail.com>
2017-06-27 10:28:55 +0900

    Revert "Restart"
    This reverts commit dc0b90ba36c964f1c47b820d0b9eb55e44f14181.

--- SimpleFunction.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define PI 3.1416
-inline double radian(double degree)
-{
-	return degree * PI / 180;
-}
--- WinMain.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 600
-
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
